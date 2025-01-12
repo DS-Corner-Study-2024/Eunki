ad5b191907e8c517c8c69c49ee4b39806e036081
eunkyoung529 <eunkyoung4160@gmail.com>
2025-01-12 23:29:01 +0900

    week11 최종

--- week11.js
+++ week11.js
@@ -21,10 +21,9 @@ const { createToken, tokenTest } = require('../controllers/v1');
 
 const router = express.Router();
 
-// POST /v1/token: 토큰을 발급하는 라우터
+
 router.post('/token', createToken);
 
-// GET /v1/test: 사용자가 토큰을 테스트해볼 수 있는 라우터
 router.get('/test', verifyToken, tokenTest);
 
 module.exports = router;
@@ -35,11 +34,11 @@ module.exports = router;
 const jwt = require('jsonwebtoken');
 const rateLimit = require('express-rate-limit');
 exports.apiLimiter = rateLimit({
-    windowMs: 60 * 1000, // 1분
+    windowMs: 60 * 1000, 
     max: 1,
     handler: function (req, res) {
         res.status(this.statusCode).json({
-            code: this.statusCode, // 기본값 429
+            code: this.statusCode, 
             message: '1분에 한 번만 요청할 수 있습니다.'
         });
     }
