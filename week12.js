e5ac01c8788e669360726a589e7f421a0dc6f5da
eunkyoung529 <eunkyoung4160@gmail.com>
2025-01-20 00:15:34 +0900

    Add files via upload

---
+++ week12.js
@@ -0,0 +1,33 @@
+// 1. HTTP
+// 2. 양방향
+// 3. Socket.IO
+// 4. 폴링 방식
+// 5. 방(room)
+// 6. 몽고디비
+// 7. 소켓 이벤트 리스너
+
+
+//8번
+//controllers/index.js
+const Room = require('../schemas/room');
+const Chat = require('../schemas/chat');
+const { removeRoom: removeRoomService } = require('../services'); 
+
+exports.removeRoom = async (req, res, next) => {
+  try {
+    await removeRoomService(req.params.id);		
+    res.send('ok');					
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+};
+
+
+//9번
+
+//app.js
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/gif', express.static(path.join(__dirname, 'uploads')));
+app.use(express.json());
